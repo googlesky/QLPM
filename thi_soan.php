2ffe2a5f540b771aba5536a759480f4fc6231292
googlesky <phuonghieuag@gmail.com>
2014-09-13 02:03:52 +0700

    Total GIT
    TOTAL

---
+++ thi_soan.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <style>
+        label{
+            background: royalblue;
+            color: #2aabd2;
+        }
+    </style>
+</head>
+<body>
+<form action="takeupload.php">
+    <label>Username</label><input type="text"/>
+    <label>Password</label><input type="password"/>
+    <input type="button" value="Check"/>
+</form>
+</body>
+</html>
