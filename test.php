50d41f45cb132f2b8bd45b9744be464859d9ddc4
googlesky <phuonghieuag@gmail.com>
2014-09-20 21:14:00 +0700

    xóa file test

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Hieu
- * Date: 13/09/14
- * Time: 05:24
- */
-
-require_once("include/function.php");
--- test3.php
+++
@@ -1,9 +0,0 @@
-<head>
-    <meta charset="utf-8">
-</head>
-
-<?php
-
-include_once("include/function.php");
-
-$Cache->set("noidungtest",sql_query("select * from users"));
